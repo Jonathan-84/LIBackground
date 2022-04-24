e62e6d19df671a51c286e3e1b07a5621655e6d85
JRob <72467031+Jonathan-84@users.noreply.github.com>
2022-04-24 11:36:25 -0400

    updated the announcment page

---
+++ assets/js/Transition.js
@@ -0,0 +1,9 @@
+function whereto(){
+    document.getElementById("role").innerHTML = " Software Engineering 👨‍💻";
+    document.getElementById('role').className = "future";
+}
+
+function wherefrom(){
+    document.getElementById("role").innerHTML = " Sales ☎️";
+    document.getElementById('role').className = "past";
+}
